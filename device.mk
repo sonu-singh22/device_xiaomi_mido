aede7c09c6ad5e41f114407d68e7940fece719c0
sonu-singh22 <rudralifesgood@gmail.com>
2024-01-25 11:56:14 +0530

    mido: adapted to superiorExt

--- device.mk
+++ device.mk
@@ -32,7 +32,7 @@ TARGET_BOARD_SUFFIX := _64
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-superior
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
@@ -505,6 +505,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Maintainer
+PRODUCT_SYSTEM_PROPERTIES += \
+       ro.spos.maintainer=Sonu_Singh
+       
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
