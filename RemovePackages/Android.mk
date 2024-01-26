6d0128a7ecf275da62582c2528147f23c4410248
sonu-singh22 <rudralifesgood@gmail.com>
2024-01-26 17:21:08 +0530

    mido: remove some packages during build time

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -6,11 +6,9 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
     AndroidAutoStub \
-    CalculatorGoogle \
-    Flipendo \
+    CalculatorGoogle  \
     GoogleFeedback \
-    GooglePartnerSetup \
-    TurboPrebuilt    
+    GooglePartnerSetup
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
