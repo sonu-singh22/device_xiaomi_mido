39eccfea7f6c0e7bf248b8d3887112f9f1d6d481
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-02-10 16:41:00 +0530

    mido: biometrics: Move fingerprint HIDL to 2.3

--- device.mk
+++ device.mk
@@ -235,7 +235,7 @@ PRODUCT_COPY_FILES += \
         
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mido \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_mido \
     fakelogprint \
     libshims_gxfpd
 
