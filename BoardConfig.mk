d2236bfa86a68b12086cfc23e3ac80dfccf80141
Bruno Martins <bgcngm@gmail.com>
2024-06-19 12:41:18 +0200

    mido: Inherit from QTI and common Xiaomi FCMs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,9 @@ LOC_HIDL_VERSION := 4.0
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := hardware/qcom-caf/common/compatibility_matrix.xml
 
